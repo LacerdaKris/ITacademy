4c944614687a33bbf0608d9afef4850004f9b6f3
Cristine Lacerda <107269748+LacerdaKris@users.noreply.github.com>
2023-03-24 08:12:03 -0300

    Update ITacademy.py

--- ITacademy.py
+++ ITacademy.py
@@ -1,7 +1,17 @@
 import pandas as pd
 from unidecode import unidecode
 
-distancias = pd.read_csv('DNIT-Distancias.csv', index_col=0, delimiter=';')
+distancias = pd.read_csv('DNIT-Distancias.csv', delimiter=';', header=None)
+# Extrai a primeira linha como lista e define como cabeçalho das colunas
+cidades = distancias.iloc[0].tolist()
+# Define a primeira coluna do DataFrame com a lista de cidades
+distancias.insert(0, 'Cidades', cidades)
+# Define a primeira coluna como índice
+distancias = distancias.set_index('Cidades')
+# Remove a primeira linha do DataFrame
+distancias = distancias.drop('')
+print(distancias)
+
 #custo por km de cada caminhão
 custo_caminhoes = {'P': 4.87, 'M': 11.92, 'G': 27.44}
 
@@ -13,27 +23,26 @@ def menu():
   print("4- Finalizar programa")
 
 opcao = 0
-while opcao != 4:
+while opcao !=4:
   menu()
   opcao = int(input("Digite a opção desejada pelo número: "))
-
   if opcao == 1:
     de = str(input("Digite a cidade de origem: "))
     origem = unidecode(de).upper()
     if origem not in distancias:
       print(f"Infelizmente ainda não temos coleta em {de}.")
-      menu()
+      continue
 
     para = str(input("Digite a cidade de destino: "))
     destino = unidecode(para).upper()
     if destino not in distancias:
       print(f"Infelizmente ainda não temos entrega em {para}.")
-      menu()
+      continue
     
     print("Capacidade máxima das opções:\nP- 1 tonelada\nM- 4 toneladas\nG- 10 toneladas")
     tamanho = str(input("Digite o tamanho do caminhão (P, M ou G): ").upper())
-    origem_posicao = distancias.index[origem]
-    destino_posicao = distancias.iloc[destino]
+    origem_posicao = distancias.index.get_loc(origem)
+    destino_posicao = distancias.iloc(destino)
     distancia = distancias.iloc[origem_posicao, destino_posicao]
     caminhao = custo_caminhoes[tamanho]
   
@@ -48,5 +57,3 @@ while opcao != 4:
     break
   else:
     print("Opção inválida! Digite um número entre 1 e 4")
-
-
